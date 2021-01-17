79872a20ac08026a2cd63cc1725a4aa4bb198d27
pinklesh parjapati <pinkleshparjapati@gmail.com>
2021-01-17 21:46:27 +0530

    bug fixed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 // import Radium from 'radium';
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import './Person.css';
+import "./Person.css";
 
 const StyledDiv = styled.div`
 width: 60%;
@@ -14,22 +14,24 @@ text-align: center;
 }
 @media (min-width: 500px) {
     width: 450px;
+}
 `;
 
-const person = ( props ) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-    return (
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
-        
-    )
+const person = (props) => {
+  const style = {
+    "@media (min-width: 500px)": {
+      width: "450px",
+    },
+  };
+  return (
+    <StyledDiv>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </StyledDiv>
+  );
 };
 
 // export default Radium(person);
